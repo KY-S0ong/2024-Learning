2c420b11a35454b85fb8c3cc19a20d664ae38fb1
KinYuan <wongky2228@gmail.com>
2024-01-27 23:02:55 -0500

    Trigger Drive Test

--- src/main/java/frc/robot/commands/ShooterCMD.java
+++ src/main/java/frc/robot/commands/ShooterCMD.java
@@ -37,7 +37,9 @@ public class ShooterCMD extends Command {
   @Override
   public void execute() {
 
-    m_subsystem.ShooterController(100*xc.getLeftY());
+    //m_subsystem.ShooterController(100*xc.getLeftY());
+    m_subsystem.ShooterController(100*xc.getRightTriggerAxis());
+    m_subsystem.ShooterController(100*xc.getLeftTriggerAxis());
 
   }
 
